9822910fa0bc343d3fe8a3dc1e7daaaa883c2626
Cameron Cabo <ccabo@seas.upenn.edu>
2017-06-24 12:12:15 -0700

    removed default files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React, {
-    Component
-} from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
-    render() {
-        return (
-          <div className = "App" >
-            <h1>"Hello, world!"</h1>
+
+  render() {
+    return (
+      <div className="app" >
+        <div className="app-header"></div>
+        <div className="app-wrapper">
+          <div className="app-nav">
+            <div className="nav screen1">
+              <p>Screen 1</p>
+            </div>
+            <div className="nav screen2">
+              <p>Screen 2</p>
+            </div>
+            <div className="nav screen3">
+              <p>Screen 3</p>
+            </div>
+
           </div>
-        );
-    }
+          <div className="main-content">
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
+ReactDOM.render(
+  <App>
+    <h1>This is a message</h1>
+  </App>,
+  document.getElementById('root')
+);
 
 export default App;
