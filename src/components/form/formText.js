5fa1e04522368f8c63bed99e4decfa204565c27f
Cameron Cabo <ccabo@seas.upenn.edu>
2017-07-04 09:22:03 -0700

    removed unnecessary functions from form text component

--- src/components/form/formText.js
+++ src/components/form/formText.js
@@ -1,29 +1,6 @@
 import React, { Component } from 'react';
 
 class FormText extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-    }
-
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-  }
-
-  handleNameChange(e) {
-    this.setState({
-      name: e.target.value,
-    });
-  }
-
-  handleEmailChange(e) {
-    this.setState({
-      email: e.target.value,
-    });
-  }
-
   render() {
     return (
       <p>
